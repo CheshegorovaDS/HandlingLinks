40f6f64b20ef237612960bd349a0784eb4d8be84
dcheshegorova <peschink@mail.ru>
2022-07-01 14:55:48 +0600

    create project

---
+++ HandlingLinksApp/app/src/main/java/ru/novikova/handlinglinksapp/AndroidAppLinksActivity.kt
@@ -0,0 +1,11 @@
+package ru.novikova.handlinglinksapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AndroidAppLinksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ HandlingLinksApp/app/src/main/java/ru/novikova/handlinglinksapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.novikova.handlinglinksapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
